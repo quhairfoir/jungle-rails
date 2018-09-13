250d544e58defde7e0ba6415bd15c7b174263232
Morag <moragstc@gmail.com>
2018-09-12 20:33:33 -0400

    pre-manual db editing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, :password, presence: true
   
   EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: EMAIL_FORMAT }
+  validates :email, presence: true, format: { with: EMAIL_FORMAT }, uniqueness: true
   
+  before_save { self.email = email.downcase }
+
 end
