a155df68c6f158debd2629ae864735231089aba9
Morag <moragstc@gmail.com>
2018-09-17 13:01:10 -0400

    added product model tests -- working

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  describe 'Validations' do
-    it {  }
+
+  context 'Validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:quantity) }
+    it { is_expected.to validate_presence_of(:category) }
+  end
+
+  context 'Relationships' do
+    it { should belong_to(:category) }
+  end
+
 end
